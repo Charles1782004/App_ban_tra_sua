e1128e4534296e0a9cdd6e6c9b2c09c62d7aca4c
Charles1782004 <phamanhquan1782004@gmail.com>
2023-12-04 22:17:07 +0700

    Du an app ban tra sua

---
+++ app/src/main/java/com/anhquan/app_ban_tra_sua/MainActivity.java
@@ -0,0 +1,14 @@
+package com.anhquan.app_ban_tra_sua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
